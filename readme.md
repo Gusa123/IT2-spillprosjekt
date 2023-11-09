e4fee6fc8a5f2c1e5defbccde8e06f8b1ce7e2be
gustavsen.ingrid@gmail.com <gustavsen.ingrid@gmail.com>
2023-11-09 13:29:25 +0100

    laget en skisse og startet meg bakrunnenog bakken på spillet

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 #spillprosjekt IT 2
 
-Todoo:
-- skisse
-- klasser
+prossesen av oppbygningen til spillet:
+1. sette opp den generelle formen for en pygame kropp, som er det som får spillet til å funke
+
+2. lage bakgrunn og bakke, og bakken skal bevege seg:
+- putte på bakrunnen i en posisjon som etterlater et sort område under som gir plass til bakken.
+- bakken putter vi i det sorte området. vi gjør at bakken flytter seg -4 til venstre hele tiden, men fordi bakken bare er et bilde vil strekene på bildet etter hvert slutte å komme og bakken blir til slutt stille. dette fikser vi ved å å si at når bakken når -35 på y-aksen så flytter bildet seg tilbake til start slik at den går evig.
+
+3. 
--- spill.py
+++ spill.py
@@ -1,1 +1,48 @@
 import pygame
+from pygame.locals import *
+
+#klasser:
+fugl
+
+#1. oppsett
+
+pygame.init()
+BREDDE=864
+HOIDE=836#jeg har tatt høyden ned 100 i forhold til vidoen
+FPS= 60
+vindu= pygame.display.set_mode((BREDDE, HOIDE)) #lager vinduet
+pygame.display.set_caption("Flappy bird") #lager en overskrift til psillet
+klokke=pygame.time.Clock()
+
+#definere spill variabler:
+bakke_skroll= 0
+skroll_speed= 4
+
+
+#bilder
+bakgrunn= pygame.image.load("bilder/bg.png")
+bakke= pygame.image.load("bilder/bakke.png")
+
+while True:
+#2. hånter input
+    for hendelse in pygame.event.get():
+        if hendelse.type== pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+            #vinduet forsvinner når du trykker kryss
+
+    #tegne bakgrunn:
+    vindu.blit(bakgrunn, (0,0))#legger inn bakrunnsbildet fra og med punkt 0,0 som er toppen vesntre hjørne
+    #tegne og skrolle bakken:
+    vindu.blit(bakke, (bakke_skroll,668))
+    bakke_skroll -=skroll_speed
+    if abs(bakke_skroll)>35: #hvis absoluttverdien til absolutt_skroll er 35, så reasetter vi bakken.
+        bakke_skroll=0
+
+
+#3. oppdater spill
+   
+#4. tegn
+
+    pygame.display.flip() #oppdaterer spillet for alt som er skrevet over
+    klokke.tick(FPS)
